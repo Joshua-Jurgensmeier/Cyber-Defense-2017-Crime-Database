3fa38c6fb90311144a8bd721094b328b5f3d9ffd
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-19 12:01:32 -0500

    ;

--- edit.php
+++ edit.php
@@ -1,5 +1,6 @@
 <?php
-	require_once __DIR__ . '/Includes/top.php'
+	require_once __DIR__ . '/Includes/top.php';
+    
     if(!(isset($_SESSION['admin']) and $_SESSION['admin'] == true))
     {
         header('Location: http://crime.team12.isucdc.com/index.php');
