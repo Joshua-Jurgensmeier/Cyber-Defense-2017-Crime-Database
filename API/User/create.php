8d76a984212a133ddd42e30aa519f1718e8198de
Joshua-Jurgensmeier <joshjurg777@gmail.com>
2017-04-20 21:49:24 -0500

    Added access control

--- API/User/create.php
+++ API/User/create.php
@@ -1,12 +1,13 @@
 <?php
-	if(!(isset($_SESSION['admin']) and $_SESSION['admin'] == true) and ($_SERVER['REQUEST_METHOD'] != "POST"))
+	
+	require_once __DIR__ . '/../../Includes/top.php';
+	require_once __DIR__ . '/../utils.php';
+
+	if(!(isset($_SESSION['admin']) and $_SESSION['admin'] == true))
 	{
 		header('Location: http://crime.team12.isucdc.com/index.php');
 		exit("Access denied");
 	}
-	
-	require_once __DIR__ . '/../../Includes/top.php';
-	require_once __DIR__ . '/../utils.php';
 
 	$_SESSION['req_data'] = parse_request();
 
--- API/User/search.php
+++ API/User/search.php
@@ -1,11 +1,13 @@
 <?php
+	
+	require_once __DIR__ . '/../../Includes/top.php';
+	require_once __DIR__ . '/../utils.php';
+
 	if(!(isset($_SESSION['admin']) and $_SESSION['admin'] == true))
 	{
 		header('Location: http://crime.team12.isucdc.com/index.php');
 		exit("Access denied");
 	}
-	require_once __DIR__ . '/../../Includes/top.php';
-	require_once __DIR__ . '/../utils.php';
 
 	$_SESSION['req_data'] = parse_request();
 
